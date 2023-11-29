5ec630c0288793c7ad375b2b5ba76766a2f7debc
Sittolls <67274019+sittollsCM@users.noreply.github.com>
2023-11-30 00:35:11 +0600

    Update RUN_bot.py

--- RUN_bot.py
+++ RUN_bot.py
@@ -74,14 +74,6 @@ def bot_startup():
             cmd = "npm start"
             Server.start_bot(cmd)
 
-            loop = 0
-            interval = 100
-            while(loop < interval):
-                loop = loop + 1
-                if (loop == interval - 1): 
-                    print("Server is still alive..")
-                    loop = 0
-
         except (KeyboardInterrupt):
             Server.shutdown_server()
 
